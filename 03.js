40619f782b88627efd3cb0960c357ae344689348
Aleksey Mikhailenko <maks.quantum@yandex.ru>
2022-08-12 23:14:01 +0300

    03

--- 03.js
+++ 03.js
@@ -4,20 +4,34 @@
 
 
 const readlineSync = require('readline-sync'); 
+let secret = Math.floor(Math.random() * (1000_000 - 100) + 100);
+// let secret = 163391;
+
 
  (function findNumber() {
-    const secretNumber = Math.floor(Math.random() * (1000_000 - 100) + 100);
-    const pickNumber = +readlineSync.question(`Put a  number?_${secretNumber}`);
-    checkNumber(pickNumber, secretNumber)
+   
+    const numLength = secret.toString().length;
+    let picked = +readlineSync.question(`Put a  number?  ${numLength}`);
+    console.log(`Вы ввели ${picked}`)
+    picked = picked.toString().split('');
+secret = secret.toString().split('');
+    if (picked.length !== secret.length) {
+        console.log('Ввели некорректное число!!!')
+      } else {
+        checkNumber(picked, secret)
+      }
+
+ 
  })();
 
  function checkNumber(picked, secret) {
-if (picked.length !== secret.length) {
-    console.log('Ввели некорректное число!!!')
-    return 'Ввели некорректное число!!!'
-}
-picked = picked.toString().split('');
-secret = secret.toString().split('');
+    
+// if (picked.length !== secret.length) {
+//   return  console.log('Ввели некорректное число!!!')
+// }
+// picked = picked.toString().split('');
+// secret = secret.toString().split('');
+let decrrisedSecretNum = secret;
 let counterRightNumAndplace = 0;
 let counterRightNum = 0;
 let setRightNumAndPlace = new Set();
@@ -27,7 +41,8 @@ let setRightNum = new Set();
 if (numP === secret[i] ) {
     setRightNumAndPlace.add(numP);
     counterRightNumAndplace++;
-    } else if (secret.indexOf(numP, i) && !setRightNum.has(numP)) {
+    decrrisedSecretNum.slice(i, i+1)
+    } else if (secret.indexOf(numP, i) <= i && !setRightNumAndPlace.has(numP) && secret.includes(numP)) {
         setRightNum.add(numP);
         counterRightNum++;
     }
@@ -36,7 +51,16 @@ if (numP === secret[i] ) {
 setRightNumAndPlace = Array.from(setRightNumAndPlace)
 setRightNum = Array.from(setRightNum)
 // setRightNum.forEach((num) => {if (setRightNumAndPlace.includes(num)) })
+
 console.log(`ответ: совпавших цифр на своих местах - ${counterRightNumAndplace} (${setRightNumAndPlace}), цифр не на своих местах - ${counterRightNum} (${setRightNum})`)
-return `ответ: совпавших цифр не на своих местах - ${counterRightNumAndplace} (${setRightNumAndPlace}), цифр на своих местах - ${counterRightNum} (${setRightNum})`
+return console.log(`Загаданное число ${secret.join('')} `)
  }
 
+/*Вы ввели 666553
+ответ: совпавших цифр на своих местах - 1 (6), цифр не на своих местах - 2 (6,3)
+Загаданное число 163391 */
+
+/*
+Вы ввели 453467
+ответ: совпавших цифр на своих местах - 0 (), цифр не на своих местах - 1 (7)
+Загаданное число 171225 */
---
+++ 03_v1.js
@@ -0,0 +1,21 @@
+// const readlineSync = require('readline-sync'); 
+// // const secretNumber = Math.floor(Math.random() * (1000_000 - 100) + 100);
+// const secretNumber = 838;
+
+// const numLength = secretNumber.toString().length;
+// // let picked = +readlineSync.question(`Put a  number?  ${numLength}`);
+// let correctRP = [];
+
+
+// function checkNumber() {
+// // let picked = picked.toString().split('');
+// let secret = secretNumber.toString().split('');
+
+// const secretNum = {}
+// for (let i = 0; i < numLength; i++) {
+//     secretNum.hasOwnProperty(secret[i]) ? secretNum[`${secret[i]}`] = [i] : secretNum[`${secret[i]}`] = secretNum[`${secret[i]}`].push(i)
+// }
+// return console.log(secretNum);
+// }
+
+// checkNumber();
